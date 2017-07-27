1d18bbc7d331e683667ee43e65e6815814cdd7e9
cmditch <cmditch@gmail.com>
2017-07-26 21:57:24 -0600

    Added retry function

--- src/Web3.elm
+++ src/Web3.elm
@@ -7,6 +7,8 @@ module Web3
 import Native.Web3
 import Task exposing (Task)
 import Web3.Internal exposing (Request)
+import Process
+import Time
 
 
 type Error
@@ -15,6 +17,35 @@ type Error
     | NoWallet
 
 
+type alias Retry =
+    { attempts : Int
+    , sleep : Float
+    }
+
+
 toTask : Request a -> Task Error a
 toTask request =
     Native.Web3.toTask request
+
+
+
+{-
+   Mad props to Nick Miller for this retry function
+-}
+
+
+retry : Retry -> Task Error a -> Task Error a
+retry { attempts, sleep } web3Task =
+    let
+        remaining =
+            attempts - 1
+    in
+        web3Task
+            |> Task.onError
+                (\x ->
+                    if remaining > 0 then
+                        Process.sleep (sleep * Time.millisecond)
+                            |> Task.andThen (\_ -> retry (Retry remaining sleep) web3Task)
+                    else
+                        Task.fail x
+                )
